5ea3086e018d611afead63e40e004920561f9acc
Haein(haemilia) <emilylee3050@gmail.com>
2025-05-22 11:24:11 +0900

    Updated git tutorial & Created git lfs tutorial. In the process of cleansing navermap data.

--- data_collection/navermap_clean_reviews.py
+++ data_collection/navermap_clean_reviews.py
@@ -24,60 +24,60 @@ id_to_name_path = OUTPUT_DIR / "restaurant_id_to_name.pkl"
 #     pickle.dump(id_to_name, wf)
 
 
-with open(id_to_name_path, "rb") as rf:
-    id_to_name = pickle.load(rf)
-with open(OUTPUT_DIR/"mapogu_yeonnamdong_naver_reviews_final.pkl", "rb") as rf:
-    reviews = pickle.load(rf)
+# with open(id_to_name_path, "rb") as rf:
+#     id_to_name = pickle.load(rf)
+# with open(OUTPUT_DIR/"mapogu_yeonnamdong_naver_reviews_final.pkl", "rb") as rf:
+#     reviews = pickle.load(rf)
 #%%
-all_rows = []
-for current_id, one_store in reviews.items():
-    for review in one_store:
-        if not review.get("id", False):
-            continue # It should have a review id in the very least. Assume faulty data if it doesn't have it.
-        try:
-            row = {}
-            row["store_id"] = current_id
-            row["store_naver_name"] = review.get("businessName")
-            row["store_name"] = id_to_name[current_id]
-            row["review_id"] = review["id"]
-            row["author_id"] = review.get("author", {}).get("id")
-            row["author_nickname"] = review.get("author", {}).get("nickname")
-            if review.get("author", {}).get("review") is not None:
-                row["author_total_reviews"] = review.get("author", {}).get("review", {}).get("totalCount")
-                row["author_total_images"] = review.get("author", {}).get("review", {}).get("imageCount")
-            else:
-                row["author_total_reviews"] = None
-                row["author_total_images"] = None
-            row["rating"] = review.get("rating")
-            row["author_page_url"] = review.get("author", {}).get("url")
-            row["review_text"] = review.get("body")
-            row["review_images"] = review.get("media", [])
-            row["visit_count"] = review.get("visitCount")
-            row["review_view_count"] = review.get("viewCount")
-            row["store_reply"] = review.get("reply", {}).get("body")
-            row["review_type"] = review.get("originType") # 영수증 or 결제 내역
-            row["purchase_item"] = review.get("item")
-            row["keyword_tags"] = review.get("votedKeywords")
-            row["reactions"] = review.get("reactionStat", {}).get("typeCount", [])
-            row["visit_keywords"] = review.get("visitCategories", [])
-            row["review_datetime"] = review.get("representativeVisitDateTime")
-        except Exception as e:
-            print("There was an error:", e)
-            print(json.dumps(review, indent=4, ensure_ascii=False))
+# all_rows = []
+# for current_id, one_store in reviews.items():
+#     for review in one_store:
+#         if not review.get("id", False):
+#             continue # It should have a review id in the very least. Assume faulty data if it doesn't have it.
+#         try:
+#             row = {}
+#             row["store_id"] = current_id
+#             row["store_naver_name"] = review.get("businessName")
+#             row["store_name"] = id_to_name[current_id]
+#             row["review_id"] = review["id"]
+#             row["author_id"] = review.get("author", {}).get("id")
+#             row["author_nickname"] = review.get("author", {}).get("nickname")
+#             if review.get("author", {}).get("review") is not None:
+#                 row["author_total_reviews"] = review.get("author", {}).get("review", {}).get("totalCount")
+#                 row["author_total_images"] = review.get("author", {}).get("review", {}).get("imageCount")
+#             else:
+#                 row["author_total_reviews"] = None
+#                 row["author_total_images"] = None
+#             row["rating"] = review.get("rating")
+#             row["author_page_url"] = review.get("author", {}).get("url")
+#             row["review_text"] = review.get("body")
+#             row["review_images"] = review.get("media", [])
+#             row["visit_count"] = review.get("visitCount")
+#             row["review_view_count"] = review.get("viewCount")
+#             row["store_reply"] = review.get("reply", {}).get("body")
+#             row["review_type"] = review.get("originType") # 영수증 or 결제 내역
+#             row["purchase_item"] = review.get("item")
+#             row["keyword_tags"] = review.get("votedKeywords")
+#             row["reactions"] = review.get("reactionStat", {}).get("typeCount", [])
+#             row["visit_keywords"] = review.get("visitCategories", [])
+#             row["review_datetime"] = review.get("representativeVisitDateTime")
+#         except Exception as e:
+#             print("There was an error:", e)
+#             print(json.dumps(review, indent=4, ensure_ascii=False))
         
 
 
-        all_rows.append(row)
-navermap_reviews = pd.DataFrame(all_rows)
+#         all_rows.append(row)
+# navermap_reviews = pd.DataFrame(all_rows)
 
 navermap_reviews_path = Path("../dataset/navermap_reviews.parquet.gzip")
-navermap_reviews.to_parquet(navermap_reviews_path, compression="gzip")
+# navermap_reviews.to_parquet(navermap_reviews_path, compression="gzip")
 
 # make "review_datetime" into timestamps
 # only leave the strings in the purchase items (lists)
 
 #%%
-focus_columns = ["purchase_item"]
+focus_columns = ["review_images"]
 navermap_reviews = pd.read_parquet(navermap_reviews_path, columns=focus_columns)
 def parse_purchase_item(purchase_item):
     if purchase_item is None:
@@ -130,13 +130,36 @@ def parse_reactions_cool(reactions):
         if reaction.get('name') == "cool":
             count = reaction.get("count")
     return count
+def parse_num_of_media(review_images):
+    return len(review_images)
+def parse_image_links(review_images):
+    image_links = []
+    for asset in review_images:
+        if not isinstance(asset, dict):
+            continue
+        if asset.get("type") == 'image':
+            image_links.append(asset.get("thumbnail"))
+    return image_links
+def parse_video_thumbnail_links(review_images):
+    # Can't access video with only video url
+    video_links = []
+    for asset in review_images:
+        if not isinstance(asset, dict):
+            continue
+        if asset.get("type") == "video":
+            video_links.append(asset.get("thumbnail"))
+    return video_links
 cleansing = {"purchase_item": parse_purchase_item,
              "keyword_tags_code": parse_keyword_tags_code,
              "keyword_tags_hangul": parse_keyword_tags_hangul,
              "reactions_fun": parse_reactions_fun,
              "reactions_helpful": parse_reactions_helpful,
              "reactions_wannago": parse_reactions_wannago,
-             "reactions_cool": parse_reactions_cool,}
+             "reactions_cool": parse_reactions_cool,
+             "num_of_media": parse_num_of_media,
+             "image_links": parse_image_links,
+             "video_thumbnail_links": parse_video_thumbnail_links,
+             }
 #%%
-navermap_reviews
+navermap_reviews["review_images"].apply(cleansing["video_thumbnail_links"])
 # %%
